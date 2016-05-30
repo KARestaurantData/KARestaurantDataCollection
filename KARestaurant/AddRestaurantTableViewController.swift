784f040bed62c00bf4ae4ebfad437f30080c4b14
Kokpheng <yinkokpheng@yahoo.com>
2016-05-30 10:34:47 +0700

    remove bs image lib

--- KARestaurant/AddRestaurantTableViewController.swift
+++ KARestaurant/AddRestaurantTableViewController.swift
@@ -12,7 +12,10 @@ import BSImagePicker
 import AssetsLibrary
 import Alamofire
 
-class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate ,UICollectionViewDelegate,UICollectionViewDataSource {
+import DKImagePickerController
+import AVKit
+
+class AddRestaurantTableViewController: UITableViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDelegate,UICollectionViewDataSource {
     
     // MARK: Properties
     
@@ -25,7 +28,19 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var assets: [DKAsset]?
     var imageArray = [UIImage]()
+    
+    struct Demo {
+        static let titles = [
+            ["Pick All", "Pick photos only", "Pick videos only", "Pick All (only photos or videos)"],
+            ["Take a picture"],
+            ["Hides camera"],
+            ["Allows landscape"],
+            ["Single select"]
+        ]
+        static let types: [DKImagePickerControllerAssetType] = [.AllAssets, .AllPhotos, .AllVideos, .AllAssets]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,6 +76,8 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
         let text = nameTextField.text ?? ""
         saveButton.enabled = !text.isEmpty
     }
+    
+    
     
     // MARK: UIImagePickerControllerDelegate
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
@@ -107,27 +124,28 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
     
     // MARK: Actions
     @IBAction func showImagePicker(sender: UITapGestureRecognizer) {
-        let vc = BSImagePickerViewController()
-        vc.maxNumberOfSelections = 6
-        
-        bs_presentImagePickerController(vc, animated: true,
-                                        select: { (asset: PHAsset) -> Void in
-                                            print("Selected image:\(asset)")
-                                            self.photoImageView.image = self.getAssetThumbnail(asset)
-                                            self.imageArray.append(self.getAssetThumbnail(asset))
-                                            print(self.imageArray.count)
-                                            
-                                            
-                                            self.collectionView.reloadData()
-                                            
-            }, deselect: { (asset: PHAsset) -> Void in
-                print("Deselected: \(asset)")
-            }, cancel: { (assets: [PHAsset]) -> Void in
-                print("Cancel: \(assets)")
-            }, finish: { (assets: [PHAsset]) -> Void in
-                self.collectionView.reloadData()
-                print("Finish: \(assets)")
-            }, completion:nil)
+        showImagePickerWithAssetType(Demo.types[0], allowMultipleType: true, sourceType: DKImagePickerControllerSourceType.Both, allowsLandscape: true, singleSelect: false);
+//        let vc = BSImagePickerViewController()
+//        vc.maxNumberOfSelections = 6
+//        
+//        bs_presentImagePickerController(vc, animated: true,
+//                                        select: { (asset: PHAsset) -> Void in
+//                                            print("Selected image:\(asset)")
+//                                            self.photoImageView.image = self.getAssetThumbnail(asset)
+//                                            self.imageArray.append(self.getAssetThumbnail(asset))
+//                                            print(self.imageArray.count)
+//                                            
+//                                            
+//                                            self.collectionView.reloadData()
+//                                            
+//            }, deselect: { (asset: PHAsset) -> Void in
+//                print("Deselected: \(asset)")
+//            }, cancel: { (assets: [PHAsset]) -> Void in
+//                print("Cancel: \(assets)")
+//            }, finish: { (assets: [PHAsset]) -> Void in
+//                self.collectionView.reloadData()
+//                print("Finish: \(assets)")
+//            }, completion:nil)
     }
     
     func reloadTable(){
@@ -136,12 +154,35 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
     
     //MARK: collection view
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArray.count
+        return self.assets?.count ?? 0
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell =  collectionView.dequeueReusableCellWithReuseIdentifier("imgcell", forIndexPath: indexPath) as! CustomCell
-        cell.myImage.image = self.imageArray[indexPath.row]
+         let asset = self.assets![indexPath.row]
+        
+//        if asset.isVideo {
+//            cell = collectionView.dequeueReusableCellWithReuseIdentifier("imgcell", forIndexPath: indexPath) as! CustomCell
+//
+//            imageView = cell?.contentView.viewWithTag(1) as? UIImageView
+//        } else {
+//            cell = collectionView.dequeueReusableCellWithReuseIdentifier("imgcell", forIndexPath: indexPath) as! CustomCell
+//
+//            imageView = cell?.contentView.viewWithTag(1) as? UIImageView
+//        }
+        
+       let cell = collectionView.dequeueReusableCellWithReuseIdentifier("imgcell", forIndexPath: indexPath) as! CustomCell
+    
+        
+        let tag = indexPath.row + 1
+
+        cell.tag = tag
+        asset.fetchOriginalImageWithCompleteBlock { (image, info) in
+            if cell.tag == tag {
+                cell.myImage.image = image
+                 self.imageArray.append(image!)
+            }
+        }
+        
         return cell
     }
     
@@ -205,7 +246,7 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
     func uploadImage(){
         
         let url = Constant.GlobalConstants.URL_BASE + "/v1/api/admin/restaurants/multiple/register"
-        
+  
         let keyJSON = "json".dataUsingEncoding(NSUTF8StringEncoding)!
         
         
@@ -215,12 +256,15 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
             headers: Constant.GlobalConstants.headers,
             multipartFormData: { multipartFormData in
                 multipartFormData.appendBodyPart(data: self.nameTextField.text!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "NAME")
+                
                 multipartFormData.appendBodyPart(data: "Hello Dest update".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "DESCRIPTION")
+                
                 multipartFormData.appendBodyPart(data: "Hello ADDD".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "ADDRESS")
                 
                 multipartFormData.appendBodyPart(data: "0".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "IS_DELIVERY")
                 
                 multipartFormData.appendBodyPart(data: "1".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "STATUS")
+                
                 multipartFormData.appendBodyPart(data: "rest cat".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "RESTAURANT_CATEGORY")
                 multipartFormData.appendBodyPart(data: "111".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "LATITUDE")
                 multipartFormData.appendBodyPart(data: "111".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "LONGITUDE")
@@ -251,7 +295,7 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
                 switch encodingResult {
                 case .Success(let upload, _, _):
                     upload.responseJSON { response in
-                        print(response.result.value)
+                        print(response)
                         //                        let responeData = Mapper<ImageUploadResponse>().map(response.result.value)
                         //
                         //                        for image in (responeData?.image)!{
@@ -288,4 +332,45 @@ class AddRestaurantTableViewController: UITableViewController,UITextFieldDelegat
     }
     
     
+    func showImagePickerWithAssetType(assetType: DKImagePickerControllerAssetType,
+                                      allowMultipleType: Bool,
+                                      sourceType: DKImagePickerControllerSourceType = .Both,
+                                      allowsLandscape: Bool,
+                                      singleSelect: Bool) {
+        
+        let pickerController = DKImagePickerController()
+        
+        // Custom camera
+        //		pickerController.UIDelegate = CustomUIDelegate()
+        //		pickerController.modalPresentationStyle = .OverCurrentContext
+        
+        pickerController.assetType = assetType
+        pickerController.allowsLandscape = allowsLandscape
+        pickerController.allowMultipleTypes = allowMultipleType
+        pickerController.sourceType = sourceType
+        pickerController.singleSelect = singleSelect
+        
+        //		pickerController.showsCancelButton = true
+        //		pickerController.showsEmptyAlbums = false
+        //		pickerController.defaultAssetGroup = PHAssetCollectionSubtype.SmartAlbumFavorites
+        
+        // Clear all the selected assets if you used the picker controller as a single instance.
+        //		pickerController.defaultSelectedAssets = nil
+        
+        pickerController.defaultSelectedAssets = self.assets
+        
+        pickerController.didSelectAssets = { [unowned self] (assets: [DKAsset]) in
+            print("didSelectAssets")
+            self.imageArray.removeAll()
+            self.assets = assets
+            self.collectionView?.reloadData()
+        }
+        
+        if UI_USER_INTERFACE_IDIOM() == .Pad {
+            pickerController.modalPresentationStyle = .FormSheet;
+        }
+        
+        self.presentViewController(pickerController, animated: true) {}
+    }
+
 }
